cefbca25d0eadbad63fe2dda10f939cdaaddae7a
Noor Muhammad <connectwithnoor1@gmail.com>
2023-04-05 13:28:56 +0500

    model, button components

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,5 +2,7 @@ import Container from "./container/Container";
 import Navbar from "./navbar/Navbar";
 import Avatar from "./user/Avatar";
 import ClientOnly from "./client/ClientOnly";
+import Modal from "./modals/Modal";
+import Button from "./button/Button";
 
-export { Navbar, Container, Avatar, ClientOnly };
+export { Navbar, Container, Avatar, ClientOnly, Modal, Button };
