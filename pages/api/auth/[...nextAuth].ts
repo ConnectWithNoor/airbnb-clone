573d6a6883cb38023880eeaad05a4c56943f4749
Noor Muhammad <connectwithnoor1@gmail.com>
2023-04-06 16:47:22 +0500

    nextAuth config

---
+++ pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,75 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import prismaClient from "@/app/libs/prismadb";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "email",
+        },
+        password: { label: "passowrd", type: "password" },
+      },
+      authorize: async (credentials) => {
+        // check if email and password exists
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prismaClient.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        // check if the user with the asked email exists and has a valid password
+        // to be used with this login method.
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        // if password provided doesnt matches the account password
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/", // in any error occurs during login, redirect to '/'
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+};
+
+export default NextAuth(authOptions);
