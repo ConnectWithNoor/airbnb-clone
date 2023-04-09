846f27a22f35d21de6e5c3ae258b2c29fa6af38f
Noor Muhammad <connectwithnoor1@gmail.com>
2023-04-09 20:33:52 +0500

    listing property

---
+++ app/api/listings/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+import prismaClient from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "Not Allowed" }, { status: 401 });
+  }
+
+  const body = await request.json();
+  const {
+    category,
+    location,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    imageSrc,
+    price,
+    title,
+    description,
+  } = body;
+
+  const listing = await prismaClient?.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing, { status: 200 });
+}
